0536c419bb64ebaf827d68895c34ac8b0a638fb2
Alex Goodship <alexgoodship@gmail.com>
2016-06-04 12:20:03 -0400

    run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+## Set working directory to location with the files ##
+setwd("~/Desktop/UCI HAR Dataset")
+library(dplyr)
+## Read in datasets from Train and Test##
+features <- read.table('./features.txt', header = FALSE)
+activityLabels <- read.table('./activity_labels.txt', header = FALSE)
+trainSubjects <- read.table('./train/subject_train.txt', header = FALSE)
+trainObservations <- read.table('./train/X_train.txt', header = FALSE)
+trainActivities <- read.table('./train/Y_train.txt', header = FALSE)
+
+testSubjects <- read.table('./test/subject_test.txt', header = FALSE)
+testObservations <- read.table('./test/X_test.txt', header = FALSE)
+testActivities <- read.table('./test/Y_test.txt', header = FALSE)
+
+##Assign names to columns##
+colnames(activityLabels) = c("activityID", "activityType")
+colnames(trainSubjects) = "subjectID"
+colnames(trainActivities) = "activityID"
+colnames(trainObservations) = features[,2]
+
+colnames(testSubjects) = "subjectID"
+colnames(testActivities) = "activityID"
+colnames(testObservations) = features[,2]
+
+##Merge into complete train and test datasets and then combine into one complete dataset##
+completeTrain <- cbind(trainSubjects, trainActivities, trainObservations)
+completeTest <- cbind(testSubjects, testActivities, testObservations)
+completeData <- rbind(completeTrain, completeTest)
+
+##Create logical vector of features containing ID, mean and standard deviation##
+columnsNeeded <- grepl(".*ID.*|.*-mean.*|.*-std.*", colnames(completeData))
+condensedData <- completeData[, columnsNeeded]
+
+##Add in descriptive activity names by merging activity labels on activity ID##
+finalData <- merge(condensedData, activityLabels, by='activityID', all.x = TRUE)
+finalData <- arrange(finalData, subjectID, activityID)
+
+##create tidy data subset with only the means of each variable and subject##
+meandataset <- aggregate(finalData[,names(finalData)!=c("activityID","subjectID", "activityType")],
+                         by=list(acitivityID=finalData$activityID, subjectID = finalData$subjectID),
+                         mean, na.rm=TRUE)
+
+##Export the dataset with the means by subject ID and acitivy##
+write.table(meandataset, "./courseprojectdataset.csv", row.names = TRUE, sep = ",")
+
